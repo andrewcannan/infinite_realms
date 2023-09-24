51b8ee2667e326e3c33f4249a8ba9731c30b01de
andrewcannan <andrewmcannan@gmail.com>
2023-09-24 16:22:54 +0000

    import stripe and set total from current bag to send to stripe

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from django.conf import settings
+import stripe
 
 
 def checkout(request):
@@ -13,6 +14,11 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
