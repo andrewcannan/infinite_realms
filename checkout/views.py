16a4cd190befaee22b797f7dea5cfcf8afcbd5ca
andrewcannan <andrewmcannan@gmail.com>
2023-09-24 16:09:30 +0000

    import stripe keys from enviroment variables and pass through view

--- checkout/views.py
+++ checkout/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from django.conf import settings
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -14,6 +18,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
     }
 
     return render(request, template, context)
