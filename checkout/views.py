9252ab25d83ba952d84e282232d165a0b102ab0d
andrewcannan <andrewmcannan@gmail.com>
2023-09-25 20:01:54 +0000

    fix typo checkout succes url and alter orderForm post logic in checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -28,39 +28,31 @@ def checkout(request):
             }
 
             order_form = OrderForm(form_data)
-            if order_form.is_valid():
-                order = order_form.save(commit=False)
-                pid = request.POST.get('client_secret').split('_secret')[0]
-                order.stripe_pid = pid
-                order.original_bag = json.dumps(bag)
-                order.save()
-                for item_id, item_data in bag.items():
-                    try:
-                        product = Product.objects.get(id=item_id)
-                        if isinstance(item_data, int):
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=item_data,
-                            )
-                            order_line_item.save()
-
-                    except Product.DoesNotExist:
-                        messages.error(request, (
-                            "One of the products in your bag wasn't "
-                            "found in our database. "
-                            "Please contact us for assistance!")
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
                         )
-                        order.delete()
-                        return redirect(reverse('view_bag'))
+                        order_line_item.save()
 
-                # Save the info to the user's profile if all is well
-                request.session['save_info'] = 'save-info' in request.POST
-                return redirect(reverse('checkout_success',
-                                        args=[order.order_number]))
-            else:
-                messages.error(request, ('There was an error with your form. \
-                    Please double check your information.'))
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag\
+                             wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[
+                order.order_number]))
 
     else:
         bag = request.session.get('bag', {})
