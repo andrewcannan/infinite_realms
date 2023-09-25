7ce668c91a34c34c9a38611aa92274bcc452c9f7
andrewcannan <andrewmcannan@gmail.com>
2023-09-25 19:45:19 +0000

    add url for checkout success

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_suscess,
+         name='checkout_success')
 ]
