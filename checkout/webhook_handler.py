d3ff99f693cbb6c8faf7b3025cc8ebacc4b24b52
andrewcannan <andrewmcannan@gmail.com>
2023-09-28 19:08:51 +0000

    fix linter error

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -100,7 +100,7 @@ class StripeWH_Handler:
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
             status=200)
-        
+
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
