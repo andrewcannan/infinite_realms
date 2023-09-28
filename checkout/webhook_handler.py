4547073db73882029a19e1395f713749338652c1
andrewcannan <andrewmcannan@gmail.com>
2023-09-28 19:15:56 +0000

    add bag and pid to order creation if not found

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -80,6 +80,8 @@ class StripeWH_Handler:
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
+                    original_bag=bag,
+                    stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
