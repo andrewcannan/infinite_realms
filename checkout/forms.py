fbd167916790da132566c2fca3a6eed89c4e1019
andrewcannan <andrewmcannan@gmail.com>
2023-09-28 20:31:59 +0000

    update county placeholder

--- checkout/forms.py
+++ checkout/forms.py
@@ -24,7 +24,7 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'town_or_city': 'Town or City',
             'county': 'County or State',
-            'country': 'Country',
+            'country': 'Country or State',
             'postcode': 'Postal Code',
         }
 
