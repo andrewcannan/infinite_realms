c74787b9071d3f75d8f6ccbabe130f11675832a4
andrewcannan <andrewmcannan@gmail.com>
2023-09-29 21:56:16 +0000

    add logic to ommit country dropdown from placeholder setting

--- profiles/forms.py
+++ profiles/forms.py
@@ -24,10 +24,12 @@ class UserProfileForm(forms.ModelForm):
 
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = ('border-black '
                                                         'rounded-0 '
                                                         'profile-form-input')
