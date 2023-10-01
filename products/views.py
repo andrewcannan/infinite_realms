def6c43b29cd168d1a3dae9f7b30e6dbe959216f
andrewcannan <andrewmcannan@gmail.com>
2023-10-01 20:20:48 +0000

    remove accidental duplicate view for product detail

--- products/views.py
+++ products/views.py
@@ -98,18 +98,6 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'products/product_detail.html', context)
-
-
 @login_required
 def add_product(request):
     """
