344ea58eb8cf63ad1d494c970d376034f830f27b
andrewcannan <andrewmcannan@gmail.com>
2023-10-01 17:10:20 +0000

    pre-populate checkout form with user profile data

--- checkout/views.py
+++ checkout/views.py
@@ -38,28 +38,27 @@ def cache_checkout_data(request):
 
 def checkout(request):
     """
-    A view to render checkout and handle form submission
+    A view to render the checkout and to handle the form submission
     """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        if request.method == 'POST':
-            bag = request.session.get('bag', {})
+        bag = request.session.get('bag', {})
 
-            form_data = {
-                'full_name': request.POST['full_name'],
-                'email': request.POST['email'],
-                'phone_number': request.POST['phone_number'],
-                'country': request.POST['country'],
-                'postcode': request.POST['postcode'],
-                'town_or_city': request.POST['town_or_city'],
-                'street_address1': request.POST['street_address1'],
-                'street_address2': request.POST['street_address2'],
-                'county': request.POST['county'],
-            }
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
 
-            order_form = OrderForm(form_data)
+        order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
@@ -76,25 +75,27 @@ def checkout(request):
                             quantity=item_data,
                         )
                         order_line_item.save()
-
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag\
-                             wasn't found in our database. "
-                        "Please call us for assistance!")
+                        "One of the products in your bag wasn't "
+                        "found in our database. "
+                        "Please contact us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
+            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[
-                order.order_number]))
-
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
+        else:
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'))
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag \
-                 at the moment")
+            messages.error(request,
+                           "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
@@ -102,16 +103,36 @@ def checkout(request):
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
-                amount=stripe_total,
-                currency=settings.STRIPE_CURRENCY,
-            )
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        # Attempt to prefill the form with any info
+        # the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, ('Stripe public key is missing. '
                                    'Did you forget to set it in '
                                    'your environment?'))
 
-    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
