8086fdc4ca81dabb4f83041ca7382ed879f48d5a
andrewcannan <andrewmcannan@gmail.com>
2023-10-01 19:40:23 +0000

    add product form

---
+++ products/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .widgets import CustomClearableFileInput
+from .models import Product, Category, Sub_category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        sub_categories = Sub_category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        sub_friendly_names = [(c.id, c.get_friendly_name()) for c in sub_categories]
+
+        self.fields['category'].choices = friendly_names
+        self.fields['sub_category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
