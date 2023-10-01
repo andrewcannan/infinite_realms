a8442de77cd2d172695826f290fff0b24d1f9a73
andrewcannan <andrewmcannan@gmail.com>
2023-10-01 20:12:54 +0000

    add view to add product

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category, Sub_category
+from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
+
+from .models import Product, Category, Sub_category
 
 
 def all_products(request):
@@ -93,3 +95,46 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
+
+@login_required
+def add_product(request):
+    """
+    Render product form and handle form submission to add a product
+    """
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry, you do not have the required permissions.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           ('Failed to add product. '
+                            'Please ensure the form is valid.'))
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
