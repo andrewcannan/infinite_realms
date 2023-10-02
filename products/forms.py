7b1b2bbb1840bf33f3ec82be34c9fc43c3324c99
andrewcannan <andrewmcannan@gmail.com>
2023-10-02 21:31:56 +0000

    add default value for category selectbox and disable it in js

--- products/forms.py
+++ products/forms.py
@@ -16,12 +16,10 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        sub_categories = Sub_category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-        sub_friendly_names = [(
-            sub_c.id, sub_c.get_friendly_name()) for sub_c in sub_categories]
 
-        self.fields['category'].choices = friendly_names
+        self.fields['category'].choices = [(
+            '', '--- Select Category ---')] + friendly_names
         self.fields['sub_category'].choices = []
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
