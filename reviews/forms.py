115d191d28f787c2c2723c79c1210c54caa8d18c
andrewcannan <andrewmcannan@gmail.com>
2023-10-03 22:11:34 +0000

    add review form

---
+++ reviews/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ('product', 'user', 'created_at',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'content': 'Place your review here'
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = ('border-black '
+                                                        'rounded-0 '
+                                                        'profile-form-input')
+            self.fields[field].label = False
