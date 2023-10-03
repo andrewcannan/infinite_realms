2bcea1ec90878f6d38efa6550df15ce5c109a4ca
andrewcannan <andrewmcannan@gmail.com>
2023-10-03 19:34:21 +0000

    make image required in form

--- products/forms.py
+++ products/forms.py
@@ -10,7 +10,7 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
 
     image = forms.ImageField(label='Image',
-                             required=False,
+                             required=True,
                              widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
