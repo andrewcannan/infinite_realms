f671039abef943ff43df55116313e755d28e2647
andrewcannan <andrewmcannan@gmail.com>
2023-10-03 19:21:10 +0000

    make product fields required

--- products/models.py
+++ products/models.py
@@ -38,12 +38,13 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sub_category = models.ForeignKey(
         'Sub_category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=False, blank=False)
+    name = models.CharField(max_length=254, null=False, blank=False)
     description = models.TextField()
-    manufacturer = models.CharField(max_length=254, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    manufacturer = models.CharField(max_length=254, null=False, blank=False)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.name
