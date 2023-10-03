60a5d7af23516e983167310c864171cf0e3337a0
andrewcannan <andrewmcannan@gmail.com>
2023-10-03 22:02:44 +0000

    add model for product reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Review(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Product Reviews'
+
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.product}'
