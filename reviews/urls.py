35e50f97963f47ace5074a315f8f23e0b2547cb9
andrewcannan <andrewmcannan@gmail.com>
2023-10-04 19:36:25 +0000

    add reviews template and add product_id to url

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reviews, name='reviews'),
-    path('add_review/<int:product_id>/', views.add_review, name='add_review')
+    path('<int:product_id>', views.reviews, name='reviews'),
+    path('add_review/<int:product_id>', views.add_review, name='add_review')
 ]
