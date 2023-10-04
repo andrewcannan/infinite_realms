c7f35c127d4f1535317642058704f0b5788ac66e
andrewcannan <andrewmcannan@gmail.com>
2023-10-04 19:18:39 +0000

    add url and view to display all reviews for product

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_review/<product_id>/', views.add_review, name='add_review')
+    path('', views.reviews, name='reviews'),
+    path('add_review/<int:product_id>/', views.add_review, name='add_review')
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .forms import ReviewForm
+from products.models import Product
+
+
+def reviews(request, product_id):
+    """
+    A view to show all reviews for product
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    reviews = Review.objects.filter(product=product)
+    template = 'reviews/reviews.html'
+
+    context = {
+        'reviews': reviews,
+        'product': product,
+    }
+
+    return render(request, template, context)
 
 
 @login_required
@@ -14,6 +32,7 @@ def add_review(request, product_id):
     form = ReviewForm()
     template = 'reviews/add_review.html'
     context = {
-        'form': form
+        'form': form,
+        'product': product,
     }
     return render(request, template, context)
