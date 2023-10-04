3ea7ca8423d5db322dd25c400e6aaca88747de16
andrewcannan <andrewmcannan@gmail.com>
2023-10-04 18:49:12 +0000

    add basic view to render form

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ReviewForm
+
+
+@login_required
+def add_review(request, product_id):
+    """
+    A view to render form and handle form submission
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    form = ReviewForm()
+    template = 'reviews/add_review.html'
+    context = {
+        'form': form
+    }
+    return render(request, template, context)
