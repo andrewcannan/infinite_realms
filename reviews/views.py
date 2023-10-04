e434ba07559192a2d96c8b58d1879363e08901e5
andrewcannan <andrewmcannan@gmail.com>
2023-10-04 22:16:37 +0000

    add logic to handle form submission

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
 
 from .forms import ReviewForm
 from products.models import Product
+from profiles.models import UserProfile
 
 
 def reviews(request, product_id):
@@ -28,8 +29,25 @@ def add_review(request, product_id):
     A view to render form and handle form submission
     """
     product = Product.objects.get(id=product_id)
+    user = UserProfile.objects.get(user=request.user)
 
-    form = ReviewForm()
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            review = form.save()
+            review.product = product
+            review.user = request.user
+            review.save()
+            messages.success(request, "Product review added successfully.")
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, "Failed to add review.\
+                            Please ensure the form is valid.")
+
+    else:
+        form = ReviewForm()
+
     template = 'reviews/add_review.html'
     context = {
         'form': form,
