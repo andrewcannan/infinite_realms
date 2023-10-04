17f78c922efc8e24f3ac266893498b8effb37aa0
andrewcannan <andrewmcannan@gmail.com>
2023-10-04 22:26:51 +0000

    import messages

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import ReviewForm
 from products.models import Product
