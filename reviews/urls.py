755600ad80b8b0cc4640452341b150e1402d46f7
andrewcannan <andrewmcannan@gmail.com>
2023-10-05 19:28:52 +0000

    add urls for edit/delete reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('<int:product_id>', views.reviews, name='reviews'),
-    path('add_review/<int:product_id>', views.add_review, name='add_review')
+    path('add_review/<int:product_id>', views.add_review, name='add_review'),
+    path('edit_review/<int:review_id>', views.edit_review, name='edit_review'),
+    path('delete_review/<int:review_id>',
+         views.delete_review, name='delete_review')
 ]
