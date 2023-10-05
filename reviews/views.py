d27797734e36131c398fc705ec240fe9acadbcec
andrewcannan <andrewmcannan@gmail.com>
2023-10-05 19:09:58 +0000

    add view to handle submission of edit review form and to render template

--- reviews/views.py
+++ reviews/views.py
@@ -55,3 +55,45 @@ def add_review(request, product_id):
         'product': product,
     }
     return render(request, template, context)
+
+
+@login_required
+def edit_review(request, review_id):
+    """
+    A view to render form and handle form submission
+    """
+    if not request.user.is_authenticated:
+        messages.error(request,
+                       'Sorry, you need to be logged in to do that.')
+        return redirect(reverse('account_login'))
+
+    review = Review.objects.get(id=review.id)
+    product = Product.objects.filter(review=review)
+    user = UserProfile.objects.get(user=request.user)
+
+    if user != review.user and not request.user.is_superuser:
+        messages.error(request, 'Sorry, you can only edit your own reviews.')
+        return redirect(reverse('home'))
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            review = form.save()
+            review.save()
+            messages.success(request, "Product review updated successfully.")
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, "Failed to update review.\
+                            Please ensure the form is valid.")
+
+    else:
+        form = ReviewForm(instance=review)
+
+    template = 'reviews/edit_review.html'
+    context = {
+        'review': review,
+        'form': form,
+        'product': product,
+    }
+    return render(request, template, context)
