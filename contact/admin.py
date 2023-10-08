eb5d1b32eedc654b3f3ac7eee4fb5b1646545b35
andrewcannan <andrewmcannan@gmail.com>
2023-10-08 16:11:27 +0000

    add contact us to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', 'email', 'reason',
+                       'content', 'created_at',)
+
+    fields = ('user', 'email', 'reason',
+              'content', 'created_at', 'response_sent')
+
+    ordering = ('-created_at',)
+
+
+admin.site.register(Contact, ContactAdmin)
