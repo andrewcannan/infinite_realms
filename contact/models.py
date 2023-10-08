7ce77fd02ec2788eb2d7702c6f8750e6353833bc
andrewcannan <andrewmcannan@gmail.com>
2023-10-08 14:37:03 +0000

    create model for contact us

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Contact Us Submissions'
+
+    user = models.ForeignKey(User, null=True, blank=True,
+                             on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    reason = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.reason}'
