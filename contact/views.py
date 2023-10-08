9a403855d0fc7203429dadc63c96ba997590ba14
andrewcannan <andrewmcannan@gmail.com>
2023-10-08 15:20:34 +0000

    add view to render contact form

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ContactForm
+
+
+@login_required
+def contact_us(request):
+    """
+    A view to render contact form and handle form submission
+    """
+    if not request.user.is_authenticated:
+        messages.error(request,
+                       'Sorry, you need to be logged in to do that.')
+        return redirect(reverse('account_login'))
+
+    form = ContactForm()
+    template = 'contact/contact_us.html'
+    context = {
+        'form': form
+    }
+    return render(request, template, context)
