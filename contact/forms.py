36e88c3d8febd5c7cf40a43eae238d51b7e9b271
andrewcannan <andrewmcannan@gmail.com>
2023-10-08 16:04:31 +0000

    add choices to reason field, add boolean field for if response sent to user

--- contact/forms.py
+++ contact/forms.py
@@ -3,38 +3,24 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
+
     class Meta:
         model = Contact
-        exclude = ('user', 'email', 'created_at',)
+        fields = ['reason', 'content']
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels
-        """
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'content': 'Leave your message here.',
-        }
+        reason_choices = [
+            ('', '--- Select Reason for Contact ---'),
+            ('products', 'Inquiries about Products'),
+            ('support', 'Customer Support or Technical Assistance'),
+            ('feedback', 'Feedback or Suggestions'),
+            ('wholesale', 'Wholesale or Bulk Orders'),
+            ('privacy', 'Privacy Concerns or Data Requests'),
+            ('marketing', 'Marketing or Advertising Inquiries'),
+            ('general', 'General Questions or Comments'),
+        ]
 
-        for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields['reason'].choices = [(
-                '', '--- Select Reason for Contact ---'),
-                ('products', 'Inquiries about Products'),
-                ('support', 'Customer Support or Technical Assistance'),
-                ('feedback', 'Feedback or Suggestions'),
-                ('wholesale', 'Wholesale or Bulk Orders'),
-                ('privacy', 'Privacy Concerns or Data Requests'),
-                ('marketing', 'Marketing or Advertising Inquiries'),
-                ('general', 'General Questions or Comments'),
-            ]
-            self.fields['reason'].widget.choices[0]['disabled'] = True
-            self.fields[field].widget.attrs['class'] = ('border-black '
-                                                        'rounded-0 '
-                                                        'profile-form-input')
-            self.fields[field].label = False
+        self.fields['reason'].choices = reason_choices
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- contact/models.py
+++ contact/models.py
@@ -10,9 +10,11 @@ class Contact(models.Model):
     user = models.ForeignKey(User, null=True, blank=True,
                              on_delete=models.CASCADE)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    reason = models.CharField(max_length=100, null=False, blank=False)
+    reason = models.CharField(max_length=100, choices=[], null=False,
+                              blank=False)
     content = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    response_sent = models.BooleanField(default=False, null=False, blank=False)
 
     def __str__(self):
         return f'{self.user} - {self.reason}'
