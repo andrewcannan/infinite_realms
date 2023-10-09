0b5e55b24b9d23978175a66aff8bfab859808df6
andrewcannan <andrewmcannan@gmail.com>
2023-10-09 21:29:26 +0000

    add static root variable

--- infinite_realms/settings.py
+++ infinite_realms/settings.py
@@ -140,20 +140,20 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator'),
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.\
-            MinimumLengthValidator'),
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'MinimumLengthValidator'),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.\
-            CommonPasswordValidator'),
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'CommonPasswordValidator'),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.\
-            NumericPasswordValidator'),
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'NumericPasswordValidator'),
     },
 ]
 
@@ -177,6 +177,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
