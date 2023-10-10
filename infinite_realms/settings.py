7916bcb61061674cf0667d55b48f64bfdae1976a
andrewcannan <andrewmcannan@gmail.com>
2023-10-10 19:32:07 +0000

    update aws bucket name and location

--- infinite_realms/settings.py
+++ infinite_realms/settings.py
@@ -190,8 +190,8 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'andrewcannan-infinite-realms'
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_STORAGE_BUCKET_NAME = 'infinite-realms'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
