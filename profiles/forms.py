4c0f32a48638bde23ac1de2ae55da01649067bc5
andrewcannan <andrewmcannan@gmail.com>
2023-10-15 17:52:46 +0000

    add aria labels and remove required placeholder attribute

--- profiles/forms.py
+++ profiles/forms.py
@@ -25,12 +25,10 @@ class UserProfileForm(forms.ModelForm):
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = ('border-black '
                                                         'rounded-0 '
                                                         'profile-form-input')
             self.fields[field].label = False
+            self.fields[field].widget.attrs['aria-label'] = placeholder
