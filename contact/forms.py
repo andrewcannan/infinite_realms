ed2e62b725b5420c63650eea6907e42b33bb7978
andrewcannan <andrewmcannan@gmail.com>
2023-10-20 19:58:19 +0000

    add response form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Contact
+from .models import Contact, Response
 
 
 class ContactForm(forms.ModelForm):
@@ -19,6 +19,28 @@ class ContactForm(forms.ModelForm):
         if self.fields['content']:
             self.fields['content'].widget.attrs['placeholder'] = (
                 'Leave your message here.')
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class ResponseForm(forms.ModelForm):
+    """
+    Class extends djangos ModelForm class
+    """
+    class Meta:
+        model = Response
+        fields = ['message']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes
+        """
+        super().__init__(*args, **kwargs)
+
+        if self.fields['content']:
+            self.fields['content'].widget.attrs['placeholder'] = (
+                'Send Response to user.')
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
