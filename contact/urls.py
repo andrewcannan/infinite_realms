9cd8030e9004f645bcfd00b17bd1aa31c4a681a0
andrewcannan <andrewmcannan@gmail.com>
2023-10-20 22:33:50 +0000

    add enquiries url

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.contact_us, name='contact_us'),
     path('send_response/<int:contact_id>', views.send_response,
-         name='send_response')
+         name='send_response'),
+    path('enquiries/', views.enquiries, name='enquiries')
 ]
