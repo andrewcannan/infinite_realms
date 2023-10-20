64c7c3581f00141000258e057e71e096d09efbd3
andrewcannan <andrewmcannan@gmail.com>
2023-10-20 20:23:12 +0000

    add url for send response view

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact_us, name='contact_us')
+    path('', views.contact_us, name='contact_us'),
+    path('/send_response', views.send_response, name='send_response')
 ]
