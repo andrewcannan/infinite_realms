efee3c0dc9347dfbb8b46c0afa39155422dffa68
andrewcannan <andrewmcannan@gmail.com>
2023-10-20 20:21:09 +0000

    add views to send a response to users contact submission

--- contact/views.py
+++ contact/views.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from .forms import ContactForm
+from .forms import ContactForm, ResponseForm
 
 
 @login_required
@@ -60,5 +60,63 @@ def contact_us(request):
     template = 'contact/contact_us.html'
     context = {
         'form': form
+    }
+    return render(request, template, context)
+
+
+@login_required
+def send_response_email(request, contact, response):
+    """
+    Send the user a response email
+    """
+    cust_email = contact.email
+    subject = render_to_string(
+        'contact/confirmation_emails/response_email_subject.txt',
+        {'contact': contact})
+    body = render_to_string(
+        'contact/confirmation_emails/response_email_body.txt',
+        {'contact': contact, 'response': response,
+         'contact_email': settings.DEFAULT_FROM_EMAIL})
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [cust_email]
+    )
+
+
+@login_required
+def send_response(request, contact_id):
+    """
+    A view to render response form and handle form submission
+    """
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry, you do not have the required permissions.')
+        return redirect(reverse('home'))
+
+    contact = Contact.objects.get(id=contact_id)
+    if request.method == "POST":
+        form = ResponseForm(request.POST)
+
+        if form.is_valid():
+            response = form.save(commit=False)
+            response.save()
+            # switch boolean field on contact to true
+            contact.response_sent = True
+            contact.save()
+            messages.success(request, "Response sent to user successfully.")
+            send_response_email(request, contact, response)
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, "Failed to send message.\
+                            Please ensure the form is valid.")
+    else:
+        form = ResponseForm()
+
+    template = 'contact/send_response.html'
+    context = {
+        'form': form,
+        'contact': contact,
     }
     return render(request, template, context)
