fbf58932d5184dd01782e4b36a8c0623a9739fb7
andrewcannan <andrewmcannan@gmail.com>
2023-10-21 20:46:41 +0000

    remove labels, add placeholders, add aria-labels

--- products/forms.py
+++ products/forms.py
@@ -20,11 +20,32 @@ class ProductForm(forms.ModelForm):
         Set choices for select dropdowns and classes
         """
         super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'category': 'Category *',
+            'sub_category': 'Sub-Category *',
+            'sku': 'Sku',
+            'name': 'Name',
+            'description': 'Description',
+            'manufacturer': 'Manufacturer',
+            'price': 'Price',
+            'image': 'Image',
+        }
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = [(
             '', '--- Select Category ---')] + friendly_names
         self.fields['sub_category'].choices = []
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0'
+            if field not in ['category', 'sub_category', 'image']:
+                self.fields[field].label = False
+                self.fields[field].widget.attrs[
+                    'placeholder'] = placeholder + '*'
+            if field in ['category', 'sub_category']:
+                self.fields[field].label = placeholder
+            if field != 'image':
+                self.fields[field].widget.attrs['aria-label'] = placeholder
