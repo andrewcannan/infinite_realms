350552c568e3a716903c2b4e8e636c70a42c9f98
andrewcannan <andrewmcannan@gmail.com>
2023-10-21 00:10:00 +0000

    add rating field to model

--- reviews/models.py
+++ reviews/models.py
@@ -17,6 +17,8 @@ class Review(models.Model):
     title = models.CharField(max_length=80, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0, validators=[
+        MinValueValidator(0), MaxValueValidator(5)])
 
     def __str__(self):
         return f'{self.user} - {self.product}'
